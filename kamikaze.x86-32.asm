288251dbc8f097e3fd28a11715fdca793d1d4ab9
m1ckey <ch.michael.krickl@gmail.com>
2020-02-05 23:21:05 +0100

    add comments

--- kamikaze.x86-32.asm
+++ kamikaze.x86-32.asm
@@ -1,10 +1,11 @@
 entry:
   mov esp, 0x3a0
   mov ecx, 0x400
+                      ; kamikaze in opcode:
   mov edi, 0x0f00fc83 ; 0x83fc000f
   mov esi, 0x6060e144 ; 0x44e16060 
   mov ebp, 0xe4ff6060 ; 0x6060ffe4
-  mov ebx, 0xffffffff
+  mov ebx, 0xffffffff ; illegal instructions
   mov edx, ebx
   mov eax, ebx
   pushad
