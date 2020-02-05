c24a0aa9edb7539b0d81301a2da3d1ec7e4548f7
m1ckey <ch.michael.krickl@gmail.com>
2020-02-06 00:53:46 +0100

    better comments

--- kamikaze.x86-32.asm
+++ kamikaze.x86-32.asm
@@ -1,11 +1,11 @@
 entry:
   mov esp, 0x3a0
-  mov ecx, 0x400
-                      ; kamikaze in opcode:
+  mov ecx, 0x400      ; upper bound
+                      ; kamikaze opcode:
   mov edi, 0x0f00fc83 ; 0x83fc000f
   mov esi, 0x6060e144 ; 0x44e16060 
   mov ebp, 0xe4ff6060 ; 0x6060ffe4
-  mov ebx, 0xffffffff ; illegal instructions
+  mov ebx, 0xffffffff ; illegal instructions to kill opponent
   mov edx, ebx
   mov eax, ebx
   pushad
